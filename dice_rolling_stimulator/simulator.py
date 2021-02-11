a195d1325f9bd3cc509a71c49b396b5bcdfc0edb
Joshuamjv2 <joshuamjv22@gmail.com>
2021-02-11 15:47:38 +0300

    code

---
+++ dice_rolling_stimulator/simulator.py
@@ -0,0 +1,48 @@
+import random
+x = "y"
+# print(random.randint(1, 6))
+
+while x == "y":
+    number = random.randint(1, 6)
+    if number == 1:
+        print("-----------")
+        print("|         |")
+        print("|    0    |")
+        print("|         |")
+        print("-----------")
+
+    elif number == 2:
+        print("-----------")
+        print("|         |")
+        print("|  0   0  |")
+        print("|         |")
+        print("-----------")
+
+    elif number == 3:
+        print("-----------")
+        print("|    0    |")
+        print("|    0    |")
+        print("|    0    |")
+        print("-----------")
+
+    elif number == 4:
+        print("-----------")
+        print("|  0   0  |")
+        print("|         |")
+        print("|  0   0  |")
+        print("-----------")
+
+    elif number == 5:
+        print("-----------")
+        print("|  0   0  |")
+        print("|    0    |")
+        print("|  0   0  |")
+        print("-----------")
+
+    elif number == 6:
+        print("-----------")
+        print("|  0   0  |")
+        print("|  0   0  |")
+        print("|  0   0  |")
+        print("-----------")
+    x = input('y or n:\n')
